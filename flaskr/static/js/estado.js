25507402f88706bf5b934ffeefecd7891cbe9672
hugarty <hp-ca@hotmail.com>
2018-07-17 18:18:00 -0300

    update
    Barbosa pediu

--- flaskr/static/js/estado.js
+++ flaskr/static/js/estado.js
@@ -1,4 +1,4 @@
-
+//paginador que leva a próxima página da pesquisa
 $('#next').on('click', function () {
     $('#next').prop('disabled', true);
     $.ajax({
@@ -29,68 +29,67 @@ $('#next').on('click', function () {
     })
   });
   
+//paginador que leva a página anterior da pesquisa
+$('#previous').on('click', function () {
+  $('#previous').prop('disabled', true);
+  $.ajax({
+    method: "GET",
+    url: ''+window.location.pathname,
+    data: {['paginador']: $('#previous').attr('name')},
+    beforeSend: antesDeEnviar(),
+    success: function (res) {
+      res.map((e, k) => {
+        paginacao(e);
+        if (e.urlFoto != null && e.urlfoto != '') {
+          return (
+            $(`
+              <a href="/deputado/${e.id}">
+              <div class="avatar"><img src="${e.urlFoto}"></div>        
+              <span class="name">${e.nome}</span>
+              </a>
+              `).appendTo('#results')
+          )
+        }
+      })
+    },
+    complete: function () {
+      $("#loader-gif").hide()
+      $('#previous').prop('disabled', false);
+    }
+  })
+});
   
+//paginador que leva a primeira página da pesquisa
+$('#first').on('click', function () {
+  $('#first').prop('disabled', true);
+  $.ajax({
+    method: "GET",
+    url: ''+window.location.pathname,
+    data: {['paginador']: $('#first').attr('name')},
+    beforeSend: antesDeEnviar(),
+    success: function (res) {
+      res.map((e, k) => {
+        paginacao(e);
+        if (e.urlFoto != null && e.urlfoto != '') {
+          return (
+            $(`
+              <a href="/deputado/${e.id}">
+              <div class="avatar"><img src="${e.urlFoto}"></div>        
+              <span class="name">${e.nome}</span>
+              </a>
+              `).appendTo('#results')
+          )
+        }
+      })
+    },
+    complete: function () {
+      $("#loader-gif").hide()
+      $('#first').prop('disabled', false);
+    }
+  })
+});
   
-  $('#previous').on('click', function () {
-    $('#previous').prop('disabled', true);
-    $.ajax({
-      method: "GET",
-      url: ''+window.location.pathname,
-      data: {['paginador']: $('#previous').attr('name')},
-      beforeSend: antesDeEnviar(),
-      success: function (res) {
-        res.map((e, k) => {
-          paginacao(e);
-          if (e.urlFoto != null && e.urlfoto != '') {
-            return (
-              $(`
-                <a href="/deputado/${e.id}">
-                <div class="avatar"><img src="${e.urlFoto}"></div>        
-                <span class="name">${e.nome}</span>
-                </a>
-                `).appendTo('#results')
-            )
-          }
-        })
-      },
-      complete: function () {
-        $("#loader-gif").hide()
-        $('#previous').prop('disabled', false);
-      }
-    })
-  });
-  
-  
-  $('#first').on('click', function () {
-    $('#first').prop('disabled', true);
-    $.ajax({
-      method: "GET",
-      url: ''+window.location.pathname,
-      data: {['paginador']: $('#first').attr('name')},
-      beforeSend: antesDeEnviar(),
-      success: function (res) {
-        res.map((e, k) => {
-          paginacao(e);
-          if (e.urlFoto != null && e.urlfoto != '') {
-            return (
-              $(`
-                <a href="/deputado/${e.id}">
-                <div class="avatar"><img src="${e.urlFoto}"></div>        
-                <span class="name">${e.nome}</span>
-                </a>
-                `).appendTo('#results')
-            )
-          }
-        })
-      },
-      complete: function () {
-        $("#loader-gif").hide()
-        $('#first').prop('disabled', false);
-      }
-    })
-  });
-  
-  
+//paginador que leva a ultima página da pesquisa
   $('#last').on('click', function () {
     $('#last').prop('disabled', true);
     $.ajax({
@@ -120,15 +119,14 @@ $('#next').on('click', function () {
     })
   });
 
-
+//deleta alguns elementos, mostra o loader e esconde os paginadores
   function antesDeEnviar (){
     deleteResultsChilds();
     $("#loader-gif").show();
     escondeBotoes ();
   }
 
-
-
+// mostra botões de paginação se necessário
 function paginacao (e){
     if(e.linkNextPage != null && e.linkNextPage != ''){
       $('#next').attr('name', e.linkNextPage);
@@ -148,6 +146,7 @@ function paginacao (e){
     }
   }
 
+//esconde os botões de paginação
 function escondeBotoes (){
     $('#next').hide();
     $('#previous').hide();
@@ -155,10 +154,10 @@ function escondeBotoes (){
     $('#last').hide();
 }
 
+//deleta resultados para colocar novos
 function deleteResultsChilds() {
   var result = document.getElementById('results');
   while (result.firstChild) {
     result.removeChild(result.firstChild);
   }
 }
-  
